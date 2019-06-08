8cfee84928a44d846da5267e2d67d4eb708f1230
KL13NT <Nabil.tharwat@outlook.com>
2019-06-08 13:51:14 +0200

    new file:   index.js 	new file:   package.json 	new file:   yarn.lock

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const DBL = require('dblapi.js')
+const dbl = new DBL('NTg2ODYxNDk2NTIxNDU3Njc0.XPuVAw.5nX0zDif84kNW55QaH4aha5vb90', client)
+
+
+dbl.on('error', e =>{
+  console.log(`Oops! ${e}`)
+})
