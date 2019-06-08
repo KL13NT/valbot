ad54453d899cace348a711facf236f084b92043b
KL13NT <Nabil.tharwat@outlook.com>
2019-06-08 14:03:09 +0200

    modified:   index.js
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 // const client = new Discord.Client()
 // const DBL = require('dblapi.js')
 // const dbl = new DBL('NTg2ODYxNDk2NTIxNDU3Njc0.XPuVAw.5nX0zDif84kNW55QaH4aha5vb90', client)
-const express = require('express').app
+const express = require('express')
 
 const app = express()
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
