05ac113eb68908b26d26df8fe23d15314aece846
KL13NT <Nabil.tharwat@outlook.com>
2019-06-08 13:53:35 +0200

    modified:   index.js 	modified:   package.json 	modified:   yarn.lock
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const DBL = require('dblapi.js')
 const dbl = new DBL('NTg2ODYxNDk2NTIxNDU3Njc0.XPuVAw.5nX0zDif84kNW55QaH4aha5vb90', client)
+const express = require('express').app
 
+const app = express()
+
+app.listen('*', (req,res)=>{
+  res.send('Hello!')
+})
 
 dbl.on('error', e =>{
   console.log(`Oops! ${e}`)
