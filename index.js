5c78c3ee4878f945b411f37a61660e9614061638
KL13NT <Nabil.tharwat@outlook.com>
2019-06-08 13:56:56 +0200

    modified:   index.js
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const DBL = require('dblapi.js')
-const dbl = new DBL('NTg2ODYxNDk2NTIxNDU3Njc0.XPuVAw.5nX0zDif84kNW55QaH4aha5vb90', client)
+// const Discord = require('discord.js')
+// const client = new Discord.Client()
+// const DBL = require('dblapi.js')
+// const dbl = new DBL('NTg2ODYxNDk2NTIxNDU3Njc0.XPuVAw.5nX0zDif84kNW55QaH4aha5vb90', client)
 const express = require('express').app
 
 const app = express()
@@ -10,6 +10,6 @@ app.listen('*', (req,res)=>{
   res.send('Hello!')
 })
 
-dbl.on('error', e =>{
-  console.log(`Oops! ${e}`)
-})
+// dbl.on('error', e =>{
+//   console.log(`Oops! ${e}`)
+// })
