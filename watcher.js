f9bcff28ac075ea92988a35ef45f5f2071edab31
KL13NT <Nabil.tharwat@outlook.com>
2019-06-10 09:29:12 +0200

    Removed multiple listeners, used RAW instead
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- watcher.js
+++ watcher.js
@@ -1,14 +1,24 @@
-var process = require('process')
-var cp = require('child_process')
-var fs = require('fs')
+const process = require('process')
+const cp = require('child_process')
+const fs = require('fs')
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
-var server = cp.fork('index.js')
+async function ls() {
+  const { stdout, stderr } = await exec('yarn jsdoc index.js')
+  stderr? console.log('JSDOC EXECUTION ERROR\n', stderr): console.log(stdout)
+}
+
+let server = cp.fork('index.js')
 console.log('Server started')
+ls()
 
 fs.watchFile('index.js', function (event, filename) {
+  ls()
   server.kill()
   console.log('Server restarting')
   server = cp.fork('index.js')
+  
 })
 
 process.on('SIGINT', function () {
