3785a26b531918646b72b690ca8cde4ef7a299df
Nabil <nabil.tharwat@outlook.com>
2020-06-15 04:08:12 +0200

    [Config] Welcome message not inclusive enough
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/listeners/NewGuildMemberListener.js
+++ src/listeners/NewGuildMemberListener.js
@@ -1,31 +1,30 @@
 const { Listener, CommandContext } = require('../structures')
 const { CLIENT_ID: BotID } = process.env
+const { log } = require('../utils/utils')
 
 class NewGuildMemberListener extends Listener {
 	constructor (client) {
 		super(client, [
 			'guildMemberAdd'
 		])
+
+		this.onGuildMemberAdd = this.onGuildMemberAdd.bind(this)
 	}
-
-
 
 	async onGuildMemberAdd (member) {
 		try{
-
 			const dm = await member.createDM()
 			dm.send(`
 			اهلاً بيكوا في فالاريوم! سعداء بيكم معانا جداً! :sparkler: :partying_face:
 			حابين انكوا تقروا القواعد الاول في تشانل #rules.
 			لو في صعوبات في التعامل مع ديسكورد او السيرفر تحديداً تقدروا تتفرجوا على الفيديو ده او تسألوا ف السيرفر: https://youtu.be/J56Ww0_GiTc
-			اهلاً بيك مره تانية, و لو في اي حاجة نقدر نساعدك فيها متترددش! اعتبرنا بيتك التاني :star_struck:
+			لو حابين تتعرفوا على الـ bot تقدروا تكتبوا \`${this.client.prefix} help\` في اي تشانل ف السيرفر
+			اهلاً بيكم مره تانية, و لو في اي حاجة نقدر نساعدكوا فيها متترددوش! اعتبرونا بيتكم التاني :star_struck:
 			`)
 
 		}
 		catch(err){
-			console.log(err)
-			const management = this.channels.find(channel => channel.id == '571741573134417920')
-			if(management) management.send('Something went wrong while greeting the new member, could yall do it for me?')
+			log(this.client, 'Something went wrong while greeting the new member, could yall do it for me?', 'error')
 		}
 	}
 
