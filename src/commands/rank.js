c47346b6f89d98215fe18801bd5889092c875c90
Nabil <nabil.tharwat@outlook.com>
2020-06-17 02:07:22 +0200

    [Refactor - WIP] Move config files to single JS config file and require it when needed
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/commands/rank.js
+++ src/commands/rank.js
@@ -2,29 +2,36 @@ const { Command, CommandOptions } = require(`../structures`)
 const { generateRankCard } = require('../utils/svg')
 const { log, getMemberObject } = require('../utils/utils')
 
+const { AUTH_VERIFIED } = require('../config/config.js').AUTH
 
-class Rank extends Command{
-	constructor (client){
+class Rank extends Command {
+	constructor(client) {
 		const commandOptions = new CommandOptions({
-      name: `rank`,
+			name: `rank`,
 			cooldown: 1000,
 			nOfParams: 0,
-			requiredRole: 'verified',
 			description: `بتشوف مستوى شخص ما`,
 			exampleUsage: `<user_id>`,
-			extraParams: true
-    })
-    super(client, commandOptions)
+			extraParams: true,
+			auth: {
+				method: 'ROLE',
+				required: AUTH_VERIFIED
+			}
+		})
+		super(client, commandOptions)
 	}
 
-	async _run(context){
-		try{
+	async _run(context) {
+		try {
 			const { message, params, channel, member: ctxMember } = context
-			const [ userMention ] = params
+			const [userMention] = params
 
-			const id = userMention? userMention.replace(/<|>|!|@/g, ''): ctxMember.user.id
+			const id = userMention
+				? userMention.replace(/<|>|!|@/g, '')
+				: ctxMember.user.id
 
-			if(id === process.env.CLIENT_ID || id === process.env.CLIENT_DEV_ID) return message.reply('متكترش هزار عشان ميتعملش عليك صريخ ضحك :"D');
+			if (id === process.env.CLIENT_ID || id === process.env.CLIENT_DEV_ID)
+				return message.reply('متكترش هزار عشان ميتعملش عليك صريخ ضحك :"D')
 
 			const member = getMemberObject(this.client, id)
 
@@ -33,8 +40,8 @@ class Rank extends Command{
 			const displayName = member.user.username.substr(0, 12) + '...'
 			const displayID = member.user.tag.split('#')[1]
 
-			const voice = res? res.voice: await RedisController.get(`VOICE:${id}`)
-			const text = res? res.text : await RedisController.get(`TEXT:${id}`)
+			const voice = res ? res.voice : await RedisController.get(`VOICE:${id}`)
+			const text = res ? res.text : await RedisController.get(`TEXT:${id}`)
 			const exp = await RedisController.get(`EXP:${id}`)
 			const level = await RedisController.get(`LEVEL:${id}`)
 
@@ -48,17 +55,18 @@ class Rank extends Command{
 				text: text || 1,
 				voice: voice || 1,
 				level: level || 1,
-				levelEXP: ((60 * Number(level) * 0.1) + 60)  || 1
+				levelEXP: 60 * Number(level) * 0.1 + 60 || 1
 			}
 
 			const card = await generateRankCard(userInfo, levelInfo)
-					message.reply("Here's the requested rank", {
-						files: [{
-							attachment: card
-						}]
-					});
-		}
-		catch(err){
+			message.reply("Here's the requested rank", {
+				files: [
+					{
+						attachment: card
+					}
+				]
+			})
+		} catch (err) {
 			console.error(err)
 			context.message.reply(err.message)
 			log(this.client, err, 'error')
