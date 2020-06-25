29d35ad2aa9448eb236897be6ab327952a262f81
Nabil <nabil.tharwat@outlook.com>
2020-06-25 06:01:39 +0200

    [Typescript] MessageListener done
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/listeners/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	ClientReadyListener: require('./ClientReadyListener'),
-	MessageListener: require('./MessageListener'),
-	VoiceListener: require('./VoiceListener'),
-	NewGuildMemberListener: require('./NewGuildMemberListener'),
-	// ReactionRolesListener: require('./ReactionRolesListener'),
-	CommandsListener: require('./CommandsListener'),
-	QueueExecuteListener: require('./QueueExecuteListener')
-};
