a87908ad85e9ac61f3eadbaecd07a8777731732e
Nabil <nabil.tharwat@outlook.com>
2020-06-25 12:27:05 +0200

    [Typescript] RedisController done
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/Controllers/RedisController.ts
+++ src/Controllers/RedisController.ts
@@ -1,67 +1,65 @@
-const { promisify } = require('util');
+import Controller from '../structures/Controller';
+import ValClient from '../ValClient';
 
-const redis = require('redis');
+import { promisify } from 'util';
 
-const { Controller } = require('../structures');
+import redis, { RedisClient } from 'redis';
+
 const { log } = require('../utils/general');
 
 export default class RedisController extends Controller {
-	constructor(client) {
+	ready: boolean = false;
+	redis: RedisClient;
+
+	constructor(client: ValClient) {
 		super(client, {
 			name: 'redis'
 		});
 		this.ready = false;
 
 		this.redis = redis.createClient(process.env.REDIS_URL);
-		this.getAsync = promisify(this.redis.get).bind(this.redis);
-
-		this.readyListener = this.readyListener.bind(this);
-		this.errorListener = this.errorListener.bind(this);
-
-		this.set = this.set.bind(this);
-		this.get = this.get.bind(this);
-		this.incr = this.incr.bind(this);
-		this.incrby = this.incrby.bind(this);
 
 		this.redis.on('ready', this.readyListener);
 		this.redis.on('error', this.errorListener);
 	}
 
-	errorListener(err) {
+	private getAsync = promisify(this.redis.get).bind(this.redis);
+	private setAsync = promisify(this.redis.set).bind(this.redis);
+	private incrAsync = promisify(this.redis.incr).bind(this.redis);
+	private incrbyAsync = promisify(this.redis.incrby).bind(this.redis);
+
+	errorListener = (err: Error) => {
 		const message = `Something went wrong when initialising Redis, ${err.message}, <@238009405176676352>`;
 
 		log(this.client, message, 'error');
 		this.redis.removeAllListeners();
-		delete this.client.controllers.redis;
-	}
+		this.ready = false;
+	};
 
-	readyListener() {
+	readyListener = () => {
 		log(this.client, 'Redis controller ready', 'info');
 		this.client.emit('queueExecute', 'Redis controller ready');
 
 		this.ready = true;
 
 		this.redis.removeListener('ready', this.readyListener);
-	}
+	};
 
-	set(key, value) {
-		if (this.ready)
-			this.redis.set(key, value, (err, ok) => {
-				if (err) log(this.client, err, 'error');
-			});
-	}
+	set = (key: string, value: string) => {
+		return this.setAsync(key, value);
+	};
 
-	get(key) {
-		if (this.ready) {
-			return this.getAsync(key);
-		} else throw Error('Redis not ready yet');
-	}
+	get = (key: string) => {
+		return this.getAsync(key);
+	};
 
-	incr(key) {
-		if (this.ready && this.redis.exists(key)) this.redis.incr(key);
-	}
+	incr = (key: string) => {
+		if (this.redis.exists(key)) return this.incrAsync(key);
+		else throw Error('Key not found');
+	};
 
-	incrby(key, by) {
-		if (this.ready && this.redis.exists(key)) this.redis.incrby(key, by);
-	}
+	incrby = (key: string, by: number) => {
+		if (this.redis.exists(key)) return this.incrbyAsync(key, by);
+		else throw Error('Key not found');
+	};
 }
