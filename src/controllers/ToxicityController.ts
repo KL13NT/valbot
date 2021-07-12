ccf5465bdb78653a3b9c9c016c86e3487b89f6d4
Nabil <nabil.tharwat@outlook.com>
2021-07-12 14:24:29 +0200

    feat: reaction handling on toxicity controller
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/controllers/ToxicityController.ts
+++ src/controllers/ToxicityController.ts
@@ -1,18 +1,42 @@
-import { Message } from "discord.js";
+import {
+	EmbedField,
+	Message,
+	MessageReaction,
+	Permissions,
+	Snowflake,
+	TextChannel,
+	User,
+} from "discord.js";
 import { ToxicityClassifier, load } from "@tensorflow-models/toxicity";
 
 import Controller from "../structures/Controller";
 import ValClient from "../ValClient";
 
-import { log } from "../utils/general";
-import { createEmbed } from "../utils/embed";
-import { getChannelObject } from "../utils/object";
+import { isDev, log } from "../utils/general";
+import {
+	createEmbed,
+	createUserModerationEmbed,
+	getEmbedField,
+	updateEmbedFields,
+} from "../utils/embed";
+import { getChannelObject, getMemberObject } from "../utils/object";
+import { mute } from "../utils/moderation";
+import { RedisController } from ".";
+
+const REACT_APPROVE_MUTE = "✅";
+
+interface ToxicityReaction {
+	message: Message;
+	user: User;
+	reportId: Snowflake;
+}
 
 export default class ToxicityController extends Controller {
-	ready = false;
-	labels: Record<string, string>;
-	threshold = 0.7;
 	classifier: ToxicityClassifier;
+	reactQueue: ToxicityReaction[];
+	labels: Record<string, string>;
+	ready = false;
+	threshold = 0.7;
 
 	constructor(client: ValClient) {
 		super(client, {
@@ -42,6 +66,7 @@ export default class ToxicityController extends Controller {
 	};
 
 	classify = async (message: Message): Promise<string[]> => {
+		if (isDev()) return Object.keys(this.labels);
 		if (!this.ready) return [];
 
 		const { content: sentence } = message;
@@ -60,18 +85,33 @@ export default class ToxicityController extends Controller {
 		);
 
 		const url = `https://discord.com/channels/${this.client.ValGuild.id}/${channel.id}/${id}`;
-		const fields = [
+		const fields: EmbedField[] = [
 			{
 				name: "Message Content",
 				value: content,
+				inline: false,
+			},
+			{
+				name: "Message ID",
+				value: id,
+				inline: false,
 			},
 			{
 				name: "Author",
 				value: `<@${author.id}>`,
+				inline: false,
 			},
 			{
-				name: "Author Member ID",
+				name: "Author ID",
 				value: `${author.id}`,
+				inline: false,
+			},
+			{
+				name: "Violations",
+				value: predictions
+					.map(prediction => this.labels[prediction])
+					.join("\n"),
+				inline: false,
 			},
 			{
 				name: "Involved Members",
@@ -79,16 +119,27 @@ export default class ToxicityController extends Controller {
 					message.mentions.members
 						.map(member => member.toString())
 						.join("\n") || "None",
+				inline: false,
 			},
 			{
-				name: "Violations",
-				value: predictions
-					.map(prediction => this.labels[prediction])
-					.join("\n"),
+				name: "Channel",
+				value: `<#${channel.id}>`,
+				inline: false,
 			},
 			{
 				name: "Message Link",
 				value: url,
+				inline: false,
+			},
+			{
+				name: "EMBED_TYPE",
+				value: "VIOLATION_REPORT",
+				inline: true,
+			},
+			{
+				name: "STATUS",
+				value: "PENDING",
+				inline: true,
 			},
 		];
 
@@ -98,11 +149,104 @@ export default class ToxicityController extends Controller {
 			url,
 			fields,
 			footer: {
-				text:
-					"You can safely ignore this report if you think it's made in error",
+				text: `React with ${REACT_APPROVE_MUTE} to approve this report. The report will be ignored in 24 hours. `,
 			},
 		});
 
-		await modLogsChannel.send(report);
+		const redis = <RedisController>this.client.controllers.get("redis");
+		const sent = await modLogsChannel.send(report);
+
+		await Promise.all([
+			redis.setAsync(`PENDING_REPORT:SPECTRE:${sent.id}`, "0"), // 0 has smaller memory footprint than empty string or other status strings
+			sent.react(REACT_APPROVE_MUTE),
+		]);
+
+		await redis.expire(
+			`PENDING_REPORT:SPECTRE:${sent.id}`,
+			24 * 60 * 60 /* 24 hours */,
+		);
+	};
+
+	react = async (reaction: MessageReaction, user: User) => {
+		await reaction.message.fetch();
+
+		const modLogsChannel = getChannelObject(
+			this.client,
+			this.client.config.CHANNEL_MOD_LOGS,
+		);
+
+		const isBot = user.id === this.client.user.id;
+		const isApproveEmoji = reaction.emoji.name === REACT_APPROVE_MUTE;
+		const hasEmbeds = reaction.message.embeds.length > 0;
+		const isModChannel = reaction.message.channel.id === modLogsChannel.id;
+
+		if (isBot || !isApproveEmoji || !isModChannel || !hasEmbeds) return;
+
+		const actor = await this.client.ValGuild.members.fetch(user.id);
+
+		const isAdmin = actor.permissions.has(Permissions.FLAGS.ADMINISTRATOR);
+		const isMod = actor.permissions.has(Permissions.FLAGS.MANAGE_ROLES);
+		const canUserMute = isAdmin || isMod;
+
+		if (!canUserMute) return;
+
+		const embed = reaction.message.embeds[0];
+		const isViolationReportEmbed = getEmbedField(
+			embed,
+			"EMBED_TYPE",
+			"VIOLATION_REPORT",
+		);
+
+		if (!isViolationReportEmbed) return;
+
+		const redis = <RedisController>this.client.controllers.get("redis");
+
+		const reportStatus = await redis.getAsync(
+			`PENDING_REPORT:SPECTRE:${reaction.message.id}`,
+		);
+
+		if (typeof reportStatus !== "string") return;
+
+		const url = `https://discord.com/channels/${this.client.ValGuild.id}/${reaction.message.channel.id}/${reaction.message.id}`;
+
+		const reason = `Spectre Report Approved by ${user.tag}. You can view the report here: ${url}`;
+		const member = getMemberObject(
+			this.client,
+			getEmbedField(embed, "author id").value,
+		);
+
+		const muteEmbed = createUserModerationEmbed({
+			title: "Muted Member",
+			member: member.id,
+			moderator: user.id,
+			channel: getEmbedField(embed, "channel").value.match(/<#(\d+)>/)[1],
+			reason,
+		});
+
+		const violationMessage = getEmbedField(embed, "message id").value;
+		const violationChannel = <TextChannel>(
+			await this.client.channels.fetch(reaction.message.channel.id)
+		);
+
+		const updatedReport = updateEmbedFields(embed, {
+			STATUS: {
+				name: "STATUS",
+				value: "FULFILLED",
+				inline: true,
+			},
+		});
+
+		await Promise.all([
+			mute(this.client, {
+				member: member.id,
+				moderator: user.id,
+				channel: reaction.message.channel.id,
+				reason,
+			}),
+			redis.del(`PENDING_REPORT:SPECTRE:${reaction.message.id}`),
+			violationChannel.messages.delete(violationMessage, reason),
+			modLogsChannel.send(`${member.toString()}`, muteEmbed),
+			reaction.message.edit(updatedReport),
+		]);
 	};
 }
