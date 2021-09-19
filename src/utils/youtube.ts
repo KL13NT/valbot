b93a675c51eedcaf42d81bd9013cbd6eb1bbcfc3
Nabil <nabil.tharwat@outlook.com>
2021-09-19 22:59:43 +0200

    fix: youtube returning mixed content
    Signed-off-by: Nabil <nabil.tharwat@outlook.com>

--- src/utils/youtube.ts
+++ src/utils/youtube.ts
@@ -1,36 +1,21 @@
 import fetch from "node-fetch";
 
-interface Snippet {
-	title: string;
-	liveBroadcastContent: "live" | "none";
-}
-
 interface VideoIdType {
 	videoId: string;
 }
 
-interface ContentDetails {
-	duration: string;
-}
-
 interface Item {
-	snippet: Snippet;
-	contentDetails: ContentDetails;
-	id?: VideoIdType;
+	id: VideoIdType;
 }
 
 export interface YouTubeResponse {
 	items: Item[];
 }
 
-const fields = ["items.id"];
-
-const YOUTUBE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&safeSearch=none&${fields.join(
-	"%2C",
-)}&key=${process.env.YOUTUBE_KEY}`;
+const YOUTUBE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&safeSearch=none&type=video&fields=items.id.videoId&key=${process.env.YOUTUBE_KEY}`;
 
 /**
- * To customise this request @see this: https://developers.google.com/youtube/v3/code_samples/code_snippets?apix_params=%7B%22part%22%3A%5B%22snippet%22%5D%2C%22id%22%3A%5B%22Ks-_Mh1QhMc%22%5D%2C%22fields%22%3A%22items.snippet.title%22%7D&apix=true
+ * To customise this request @see this: https://developers.google.com/youtube/v3/code_samples/code_snippets?apix=true&apix_params=%7B%22part%22%3A%5B%22snippet%22%5D%2C%22maxResults%22%3A1%2C%22order%22%3A%22relevance%22%2C%22q%22%3A%22the%20great%20wall%20ost%22%2C%22safeSearch%22%3A%22none%22%2C%22type%22%3A%5B%22video%22%5D%2C%22fields%22%3A%22items.id.videoId%22%2C%22prettyPrint%22%3Afalse%7D
  */
 export const searchVideoMeta = (query: string): Promise<YouTubeResponse> =>
 	fetch(`${YOUTUBE_URL}&q=${encodeURIComponent(query)}`, {
