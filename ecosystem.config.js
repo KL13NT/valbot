46ab27d1c62239abaef687cb782f956e3c7ee440
Nabil Tharwat <nabil.tharwat@outlook.com>
2022-12-31 22:49:35 +0200

    fix: pm2 env
    Signed-off-by: Nabil Tharwat <nabil.tharwat@outlook.com>

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+const prodEnv = dotenv.config({ path: "./.env.prod" });
+const devEnv = dotenv.config({ path: "./.env.dev" });
+
+console.log({
+	MODE: "PRODUCTION",
+	...prodEnv.parsed,
+});
+
+module.exports = {
+	apps: [
+		{
+			name: "valbot",
+			script: "./prod/index.js",
+			watch: false,
+			env_dev: {
+				MODE: "DEVELOPMENT",
+				...devEnv.parsed,
+			},
+			env_prod: {
+				MODE: "PRODUCTION",
+				...prodEnv.parsed,
+			},
+		},
+	],
+};
